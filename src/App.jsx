0460afe9eec720ea4410bed970aac91a4e6db4b2
G808 <hucciallstars@gmail.com>
2023-01-17 22:47:42 +0200

    Added router, react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AddNote from './pages/AddNote';
+import EditNote from './pages/EditNote';
+import Notes from './pages/Notes';
+
 function App() {
-  return <div className='App'>hi</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Notes />} />
+        <Route path='/add-note' element={<AddNote />} />
+        <Route path='/edit-note/:id' element={<EditNote />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Notes.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Notes = () => {
+  return (
+    <section>
+      <header className='notes__header'>
+        <h2>Notes</h2>
+        <input type='text' placeholder='Search...' autoFocus />
+        <button></button>
+      </header>
+    </section>
+  );
+};
+
+export default Notes;
