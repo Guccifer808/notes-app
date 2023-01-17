01e8b4d40d63fc47e7f7abc8af5b6c0e81ca0012
G808 <hucciallstars@gmail.com>
2023-01-17 23:53:37 +0200

    Thats it for today ;)

---
+++ src/mockData.js
@@ -0,0 +1,21 @@
+const mockNotes = [
+  {
+    id: 1,
+    title: 'lorem ipsum dolor sit amet, consectetur adip',
+    body: 'Lorem ipsum dolor sit amet, consectet ipsum dolor sit amet, consectet ipsum dolor sit amet, consectet ipsum dolor sit amet, consectet ipsum dolor sit amet, consectet',
+    date: 'Jan 1, 2023',
+  },
+  {
+    id: 2,
+    title: 'lorem ipsum dolor sit amet',
+    body: 'Lorem ipsum dolor sit amet,  consectet ipsum dolor sit amet, consectet ipsum dolor sit amet, consectet ipsum dolor sit amet, consectet',
+    date: 'Jan 2, 2023',
+  },
+  {
+    id: 3,
+    title: 'lorem ipsum',
+    body: 'Lorem ipsum dolor sit amet, consectet ipsum dolor sit amet, consectet',
+    date: 'Jan 3, 2023',
+  },
+];
+export default mockNotes;
--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiSearch } from 'react-icons/fi';
+import { BsPlusLg } from 'react-icons/bs';
+import mockNotes from '../mockData';
+import NoteItem from '../components/NoteItem';
 
 const Notes = () => {
   return (
     <section>
+      {/* Header */}
       <header className='notes__header'>
         <h2>Notes</h2>
-        <input type='text' placeholder='Search...' autoFocus />
-        <button></button>
+        <input type='text' placeholder='Find notes...' autoFocus />
+        <button className='btn'>
+          <FiSearch />
+        </button>
       </header>
+      {/* Notes  */}
+      <div className='notes__container'>
+        {mockNotes.map((note) => (
+          <NoteItem key={note.id} />
+        ))}
+      </div>
+      {/* Add Note button */}
+      <Link className='btn add__btn'>
+        <BsPlusLg />
+      </Link>
     </section>
   );
 };
